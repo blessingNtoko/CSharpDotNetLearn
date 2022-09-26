c6498e992cae45429cdd5ae240fe4ffd0fa43f1f
Gundo Ntoko <gundo@purplecosmos.co.za>
2022-09-26 11:13:09 +0200

    adding functionality

--- Program.cs
+++ Program.cs
@@ -22,6 +22,59 @@ namespace CSharpDotNetLearn
 
             // Reset Text Colour
             Console.ResetColor();
+
+            // Ask users name
+            Console.WriteLine("What is your name: ");
+
+            // Get user input
+            string? inputName = Console.ReadLine();
+
+            Console.WriteLine($"Hello {inputName}, let's play a game...");
+
+            // Set correct number
+            int correctNumber = 7;
+
+            // Init guess var
+            int guess = 0;
+
+            // Ask user for number
+            Console.WriteLine("Guess a number between 1 and 10");
+
+            // While guess is not correct
+            while(guess != correctNumber)
+            {
+                // Get users input
+                string? input = Console.ReadLine();
+
+                // Cast to int and put in guess
+                guess = Convert.ToInt32(input);
+
+                // Match guess to correct number
+                if (guess != correctNumber)
+                {
+                    // Change text colour
+                    Console.ForegroundColor = ConsoleColor.Red;
+
+                    // Tell user it was the wrong number
+                    Console.WriteLine("Your guess is incorrect, try again");
+
+                    // Reset Text Colour
+                    Console.ResetColor();
+
+                }
+            }
+
+            // Output success message
+
+            // Change text colour
+            Console.ForegroundColor = ConsoleColor.Yellow;
+
+            // Tell user they won
+            Console.WriteLine("You got it right!! Well Done");
+
+            // Reset Text Colour
+            Console.ResetColor();
+
         }
     }
 }
